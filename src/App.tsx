7368452143d25e94bc23121e6012b8d83d65ad26
Lucian AF <lucian_af@hotmail.com>
2022-08-07 19:31:35 -0300

    feat: adicionado rotas para a aplicação;

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  const [onOff, setOnOff] = useState<"on" | "off">("off");
-
-  function handleOnOff() {
-    setOnOff(onOff === "off" ? "on" : "off");
-  }
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button onOff={onOff} onClick={handleOnOff} />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
