1d198fc6bbe212d06d75702951328483c175391e
Lucian AF <lucian_af@hotmail.com>
2022-08-08 21:36:52 -0300

    feat: concluído estrutura e estilo da página home;

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from "./styles";
 
 export function Home() {
@@ -12,9 +15,29 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto X" />
+          </datalist>
+
           <label htmlFor="minuteAmount">durante</label>
-          <input type="number" id="minuteAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minuteAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
           <span>minutos.</span>
         </FormContainer>
 
@@ -26,10 +49,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -28,6 +28,39 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `;
 
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme.GRAY_5};
+
+  font-weight: bold;
+  font-size: ${(props) => props.theme.FONT_LG};
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme.GRAY_1};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme.GREEN_5};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme.GRAY_5};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`;
+
 export const CountdownContainer = styled.div`
   font-family: "Roboto Mono", monospace;
   font-size: ${(props) => props.theme.FONT_XXL};
@@ -55,4 +88,29 @@ export const Separator = styled.div`
   justify-content: center;
 `;
 
-export const Button = styled.button``;
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+  background: ${(props) => props.theme.GREEN_5};
+  color: ${(props) => props.theme.GRAY_1};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme.GREEN_7};
+  }
+`;
